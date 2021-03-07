1a6dcbae7e1b8cb45fd210eaf4188edfd6068f57
Dan Williams <git@iamdanw.com>
2021-03-07 19:44:12 +0000

    Add script to fetch current age criteria
    Initially I want to be notified of what the current age criteria is.
    
    Here's a simple script to naively extract the age and print it out.
    
    Next I'll be piping this in to twitter. Twitter rejects duplicate tweets
    so for now I do not have to persist anything.

---
+++ script/get_current_age_criteria.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+require('./lib/app/criteria_extractor')
+
+NHS_ENGLAND_VACCINATION_SITE = 'https://www.nhs.uk/conditions/coronavirus-covid-19/coronavirus-vaccination/book-coronavirus-vaccination/'
+
+page = URI.parse(NHS_ENGLAND_VACCINATION_SITE).open.read
+
+criteria = CriteriaExtractor.new(page).criteria
+
+puts criteria.filter { |item| item.include?('age') }.first
