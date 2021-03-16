c982a2375b5b7e0ed323236259f172fcdf9b7965
Dan Williams <git@iamdanw.com>
2021-03-16 22:52:32 +0000

    wip make rubocop be happy

--- lib/app/criteria_store.rb
+++ lib/app/criteria_store.rb
@@ -1,21 +1,22 @@
 # frozen_string_literal: true
 
-
 class CriteriaStore
   def initialize(json)
     @json = json
   end
 
   def latest
-    @json[:history].sort{ |a, b|
+    @json[:history].max do |a, b|
       a[:captured_at] <=> b[:captured_at]
-    }.last
+    end
   end
 
   def add(new_criteria)
-    @json[:history].push({
-      captured_at: Time.now.utc.to_i,
-      criteria: new_criteria
-    })
+    @json[:history].push(
+      {
+        captured_at: Time.now.utc.to_i,
+        criteria: new_criteria
+      }
+    )
   end
 end
--- spec/app/criteria_store_spec.rb
+++ spec/app/criteria_store_spec.rb
@@ -4,64 +4,65 @@ require('app/criteria_store')
 require('timecop')
 
 RSpec.describe CriteriaStore do
-  let(:example_json) {
+  subject(:criteria_store) { described_class.new(example_json) }
+
+  let(:example_json) do
     {
       history: [
         {
-          captured_at: 1615531654,
+          captured_at: 1_615_531_654,
           criteria: [
-            "you are aged 55 or over",
-            "you are at high risk from coronavirus (clinically extremely vulnerable)",
-            "you are an eligible frontline health or social care worker",
-            "you have a condition that puts you at higher risk (clinically vulnerable)",
-            "you have a learning disability",
-            "you are a main carer for someone at high risk from coronavirus"
+            'you are aged 55 or over',
+            'you are at high risk from coronavirus (clinically extremely vulnerable)',
+            'you are an eligible frontline health or social care worker',
+            'you have a condition that puts you at higher risk (clinically vulnerable)',
+            'you have a learning disability',
+            'you are a main carer for someone at high risk from coronavirus'
           ]
         },
         {
-          captured_at: 1615500000,
+          captured_at: 1_615_500_000,
           criteria: [
-            "you are aged 60 or over",
-            "you are at high risk from coronavirus (clinically extremely vulnerable)",
-            "you are an eligible frontline health or social care worker",
-            "you have a condition that puts you at higher risk (clinically vulnerable)",
-            "you are a main carer for someone at high risk from coronavirus"
+            'you are aged 60 or over',
+            'you are at high risk from coronavirus (clinically extremely vulnerable)',
+            'you are an eligible frontline health or social care worker',
+            'you have a condition that puts you at higher risk (clinically vulnerable)',
+            'you are a main carer for someone at high risk from coronavirus'
           ]
         }
       ]
     }
-  }
-  subject(:criteria_store) { described_class.new(example_json) }
+  end
 
   describe '#latest' do
     it 'returns the most recently captured criteria' do
-      expect(subject.latest[:captured_at]).to eq 1615531654
+      expect(criteria_store.latest[:captured_at]).to eq 1_615_531_654
     end
   end
 
   describe '#add' do
-    let(:new_criteria) {
+    let(:new_criteria) do
       [
-        "you are aged 40 or over",
-        "you are at high risk from coronavirus (clinically extremely vulnerable)",
-        "you are an eligible frontline health or social care worker",
-        "you have a condition that puts you at higher risk (clinically vulnerable)",
-        "you have a learning disability",
-        "you are a main carer for someone at high risk from coronavirus",
-        "you are in a higher risk profession"
+        'you are aged 40 or over',
+        'you are at high risk from coronavirus (clinically extremely vulnerable)',
+        'you are an eligible frontline health or social care worker',
+        'you have a condition that puts you at higher risk (clinically vulnerable)',
+        'you have a learning disability',
+        'you are a main carer for someone at high risk from coronavirus',
+        'you are in a higher risk profession'
       ]
-    }
+    end
 
     it 'stores the captured_at utc timestamp' do
       Timecop.freeze(2021, 3, 14) do
-        subject.add(new_criteria)
-        expect(subject.latest[:captured_at]).to eq 1615680000
+        criteria_store.add(new_criteria)
+        expect(criteria_store.latest[:captured_at]).to eq 1_615_680_000
       end
     end
 
     it 'stores the criteria' do
-      subject.add(new_criteria)
-      expect(subject.latest[:criteria]).to match_array(new_criteria)
+      criteria_store.add(new_criteria)
+      expect(criteria_store.latest[:criteria]).to match_array(new_criteria)
     end
   end
 end
