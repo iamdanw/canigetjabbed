d1ff9d773b2a86a32f12bfa7a1ca4d765f136967
Dan Williams <git@iamdanw.com>
2021-03-18 12:30:00 +0000

    Move tweeting to another file
    I am splitting 'fetching the criteria' and 'posting a tweet' into
    separate scripts. This will give me better clarity on the github actions
    run overview.

--- script/get_current_age_criteria.rb
+++ script/get_current_age_criteria.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
-require 'dotenv/load'
 require 'open-uri'
-require 'twitter'
 
 require('./lib/app/criteria_extractor')
 
 NHS_ENGLAND_VACCINATION_SITE = 'https://www.nhs.uk/conditions/coronavirus-covid-19/coronavirus-vaccination/book-coronavirus-vaccination/'
-TWEETING_ENABLED = ENV.fetch('TWEETING_ENABLED', 'disabled')
 
 page = URI.parse(NHS_ENGLAND_VACCINATION_SITE).open.read
 
@@ -16,16 +13,3 @@ criteria = CriteriaExtractor.new(page).criteria
 age_criteria = criteria.filter { |item| item.include?('age') }.first
 
 puts age_criteria
-
-exit unless TWEETING_ENABLED == 'enabled'
-
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['API_KEY']
-  config.consumer_secret     = ENV['API_KEY_SECRET']
-  config.access_token        = ENV['ACCESS_TOKEN']
-  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
-end
-
-tweet_text = "Now booking! You can now book a coronavirus vaccination appointment in England if:\n\n- #{age_criteria}"
-
-client.update(tweet_text)
---
+++ script/tweet_current_age_criteria.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'dotenv/load'
+require 'twitter'
+
+TWEETING_ENABLED = ENV.fetch('TWEETING_ENABLED', 'disabled')
+
+age_criteria = ARGV[0]
+
+exit unless TWEETING_ENABLED == 'enabled'
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV['API_KEY']
+  config.consumer_secret     = ENV['API_KEY_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+tweet_text = "Now booking! You can now book a coronavirus vaccination appointment in England if:\n\n- #{age_criteria}"
+
+client.update(tweet_text)
+
+puts 'I just tweeted it out:'
+puts tweet_text
